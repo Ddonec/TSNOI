563fd5d38701efc6b3d42f56fe5ece7e190033f4
ddonec <rolldonec@gmail.com>
2024-08-16 19:42:47 +0300

    feat: test scroll in page

--- JS/index.js
+++ JS/index.js
@@ -15,3 +15,40 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+    const container = document.querySelector('.ways-second-container');
+
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
+
+    // Функция для начала прокрутки
+    function startScroll(e) {
+      isDragging = true;
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+      container.style.cursor = 'grabbing'; // Устанавливаем курсор, когда перетаскиваем
+      console.log("get")
+    }
+
+    // Функция для прокрутки
+    function scrolling(e) {
+      if (!isDragging) return;
+      e.preventDefault();
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 2; // Скорость прокрутки
+      container.scrollLeft = scrollLeft - walk;
+      console.log(walk)
+    }
+
+    // Функция для завершения прокрутки
+    function endScroll() {
+      isDragging = false;
+      container.style.cursor = 'grab'; // Возвращаем курсор в исходное состояние
+      console.log('push')
+    }
+
+    // Добавляем обработчики событий
+    container.addEventListener('mousedown', startScroll);
+    container.addEventListener('mousemove', scrolling);
+    container.addEventListener('mouseup', endScroll);
+    container.addEventListener('mouseleave', endScroll); // Для случая, когда мышь уходит за пределы контейнера
